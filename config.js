8624d8f3d37242ee32972c84209a00ddb6cdc4da
xDelly99 <46676092+xDelly99@users.noreply.github.com>
2024-08-23 01:00:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 import moment from 'moment-timezone';
 
 global.owner = [
-  ['390371717717', 'nobody|'],
+  ['212690494101', 'BotWhaIta|'],
   ['393518398856', 'nobody|'],
   ['393755775755', 'nobody|'],
   ['393755575577', 'nobody|'],
